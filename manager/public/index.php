5affcb26b33cfc8b495359bbb40d951bd92c3576
antontit <antontitov718@gmail.com>
2020-05-17 17:26:37 +0300

    added psr-7

--- manager/public/index.php
+++ manager/public/index.php
@@ -1,11 +1,28 @@
 <?php
 
-echo "<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
-	<h1>Hello world!<h1>
-</body>
-</html>";
+use Slim\Factory\AppFactory;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+chdir(dirname(__DIR__));
+require 'vendor/autoload.php';
+
+http_response_code(500);
+
+(function () {
+
+    $app = AppFactory::create();
+	$app->addRoutingMiddleware();
+
+	$isDebug = (bool)getenv('APP_DEBUG');
+	$errorMiddleware = $app->addErrorMiddleware($isDebug, $isDebug, $isDebug);
+
+	$app->get('/', function (Request $request, Response $response) {
+		$payload = json_encode(['content' => 'hello world']);
+		$response->getBody()->write($payload);
+		return $response
+          ->withHeader('Content-Type', 'application/json');
+	});
+
+	$app->run();
+})();
